8de44abd9b2de9762de04d84b5781ecd162b7722
Abhinay Omkar <abhiomkar@gmail.com>
2023-05-20 10:14:02 +0530

    Add link to api key page in options page

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -2,8 +2,8 @@ import { h, render, FunctionComponent } from 'preact';
 
 const Options: FunctionComponent<{apiKey: string}> = ({apiKey}) => {
   return (
-    <div class="px-4 h-full bg-gray-100">
-      <div class="max-w-xl mx-auto border-x px-16 py-8 h-full bg-gray-50">
+    <div class="px-4 h-full bg-gray-50">
+      <div class="max-w-xl mx-auto border-x px-16 py-8 h-full bg-white">
         <div>
           <h1 class="text-2xl font-semibold text-gray-900">C-GPT Options</h1>
         </div>
@@ -20,6 +20,9 @@ const Options: FunctionComponent<{apiKey: string}> = ({apiKey}) => {
               class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
               />
           </div>
+        </div>
+        <div class="pt-4">
+          <div class="text-sm">Get an OpenAI <a href="https://platform.openai.com/account/api-keys" target="_blank" class="text-sm font-medium leading-6 text-blue-600 hover:text-blue-500">API key</a> and paste it above.</div>
         </div>
       </div>
     </div>
